a731d805dcf7f366815375b8c121695a6d7cedfe
cpy0126 <0126morris@gmail.com>
2021-06-16 11:35:06 +0800

    merge.cpp

--- findSimilarTest/merge.cpp
+++ findSimilarTest/merge.cpp
@@ -4,28 +4,29 @@ using namespace std;
 int main()
 {
     ofstream fout;
-    fout.open("test.c");
-    ifstream f;
+    string filename;
+    cin>>filename;
+    fout.open("main.c");
+    ifstream f,a;
     string buf;
-    f.open("start.txt");
+    f.open(filename);
+    a.open("../sim_AND.txt");
     while(getline(f,buf)){
-        fout<<buf<<endl;
+        if(buf=="static char *sim_s=\"abc\";"){
+            fout<<"const char *sim_s=\"";
+            a>>buf;
+            string nbuf="";
+            //control the size of the table
+            //20000*10000 == table[10000][10000]
+            for(int i=0;i<20000*10000;i++)nbuf+=buf[i];
+            fout<<nbuf;
+            f.close();
+            fout<<"\";"<<endl;
+        }
+        else
+            fout<<buf<<endl;
     }
     f.close();
-    fout<<"const char *sim_s=\"";
-    f.open("../sim_AND.txt");
-	f>>buf;
-	//control the size of the table
-	//20000*10000 == table[10000][10000]
-	string nbuf="";
-	for(int i=0;i<20000*10000;i++)nbuf+=buf[i];
-	fout<<nbuf;
-    f.close();
-    fout<<"\";"<<endl;
-    f.open("end.txt");
-    while(getline(f,buf)){
-        fout<<buf<<endl;
-    }
-    f.close();
+    a.close();
 }
 
