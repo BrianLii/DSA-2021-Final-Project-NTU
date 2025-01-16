2e2591a92be5dd2cf020be65a82d5d21ba596041
Brian Li <brianlee0415@gmail.com>
2025-01-17 01:02:38 +0800

    feat: use single quote for python

--- get_token_sizes.py
+++ get_token_sizes.py
@@ -1,12 +1,12 @@
 import sys
 from dsa_mail.token import get_tokens_size, unique
 
-if __name__ == "__main__":
-    input_prefix = "test_data/mail"
+if __name__ == '__main__':
+    input_prefix = 'test_data/mail'
     num_files = 10000
     log_freq = 1000
     all_tokens = []
     for i in range(1, num_files + 1):
-        print(get_tokens_size(f"{input_prefix}{i}"), end=" ")
+        print(get_tokens_size(f'{input_prefix}{i}'), end=' ')
         if i % log_freq == 0:
-            print(f"{i}/{num_files} completed", file=sys.stderr)
+            print(f'{i}/{num_files} completed', file=sys.stderr)
--- my_replace.py
+++ my_replace.py
@@ -1,28 +1,28 @@
 def to_c_array(data):
-    return "{" + ",".join(data) + "}"
+    return '{' + ','.join(data) + '}'
 
 
-if __name__ == "__main__":
-    source_name = "main.c"
+if __name__ == '__main__':
+    source_name = 'main.c'
 
-    with open("token_sizes.txt", "r") as token_sizes:
+    with open('token_sizes.txt', 'r') as token_sizes:
         token_sizes = token_sizes.read().split()
         TOKEN_SET_SIZE_INIT = to_c_array(token_sizes)
 
-    with open("sender_receiver.txt", "r") as sender_receiver:
+    with open('sender_receiver.txt', 'r') as sender_receiver:
         ids = sender_receiver.read().split()
         sender_id, receiver_id = ids[::2], ids[1::2]
         FROM_HASH_INIT = to_c_array(sender_id)
         TO_HASH_INIT = to_c_array(receiver_id)
 
-    with open("sim_and.txt", "r") as sim_and:
+    with open('sim_and.txt', 'r') as sim_and:
         SIM_S_INIT = sim_and.read()
 
-    with open(source_name, "r") as source:
+    with open(source_name, 'r') as source:
         source = source.read()
-        source = source.replace("TOKEN_SET_SIZE_INIT", TOKEN_SET_SIZE_INIT)
-        source = source.replace("FROM_HASH_INIT", FROM_HASH_INIT)
-        source = source.replace("TO_HASH_INIT", TO_HASH_INIT)
-        source = source.replace("SIM_S_INIT", SIM_S_INIT)
+        source = source.replace('TOKEN_SET_SIZE_INIT', TOKEN_SET_SIZE_INIT)
+        source = source.replace('FROM_HASH_INIT', FROM_HASH_INIT)
+        source = source.replace('TO_HASH_INIT', TO_HASH_INIT)
+        source = source.replace('SIM_S_INIT', SIM_S_INIT)
 
     print(source)
