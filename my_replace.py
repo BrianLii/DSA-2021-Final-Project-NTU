4f95ee4b791f12b6cddf1414dbe12dd2b6fba230
Brian Li <brianlee0415@gmail.com>
2025-01-17 01:00:06 +0800

    feat: put sim_and table into my_replace

--- my_replace.py
+++ my_replace.py
@@ -5,22 +5,24 @@ def to_c_array(data):
 if __name__ == "__main__":
     source_name = "main.c"
 
-    token_sizes_filename = "token_sizes.txt"
-    with open(token_sizes_filename, "r") as token_sizes:
+    with open("token_sizes.txt", "r") as token_sizes:
         token_sizes = token_sizes.read().split()
         TOKEN_SET_SIZE_INIT = to_c_array(token_sizes)
 
-    sender_receiver_filename = "sender_receiver.txt"
-    with open(sender_receiver_filename, "r") as sender_receiver:
+    with open("sender_receiver.txt", "r") as sender_receiver:
         ids = sender_receiver.read().split()
         sender_id, receiver_id = ids[::2], ids[1::2]
         FROM_HASH_INIT = to_c_array(sender_id)
         TO_HASH_INIT = to_c_array(receiver_id)
+
+    with open("sim_and.txt", "r") as sim_and:
+        SIM_S_INIT = sim_and.read()
 
     with open(source_name, "r") as source:
         source = source.read()
         source = source.replace("TOKEN_SET_SIZE_INIT", TOKEN_SET_SIZE_INIT)
         source = source.replace("FROM_HASH_INIT", FROM_HASH_INIT)
         source = source.replace("TO_HASH_INIT", TO_HASH_INIT)
+        source = source.replace("SIM_S_INIT", SIM_S_INIT)
 
     print(source)
